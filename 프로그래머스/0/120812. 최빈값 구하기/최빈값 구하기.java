82997f4e33564154cacf86a5950cdd295bbe9a0b
2taeyeop <102136008+2taeyeop@users.noreply.github.com>
2025-04-28 14:03:54 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.24 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        int[] count = new int[array.length];
+        
+        // 중복 횟수 count 배열에 추가
+        for(int i = 0; i < array.length; i++){
+            count[i] = 0;
+            for(int j = 0; j < array.length; j++){
+                if(array[i] == array[j]){
+                    count[i]++;
+                }
+            }
+        }
+        
+        // 최빈값 인덱스 찾기
+        int max = 0;
+        int idx = 0;
+        boolean mode = false;
+        for(int l = 0; l < count.length; l++){
+            if(max < count[l]){
+                 max = count[l];
+                 idx = l;
+                 answer = array[l];
+            }
+            else if(count[l] == max && array[l] != array[idx]){
+                answer = -1;
+            }
+        }
+        
+        // for(int m = 0; m < count.length - 1; m++){
+        //     if(count[m] == count[idx] && array[m] != array[m + 1]){
+        //         answer = -1;
+        //     }
+        // }
+        
+        return answer;
+    }
+}
