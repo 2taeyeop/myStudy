3699f3e0639e365445933c626b0a567b129e04b2
2taeyeop <102136008+2taeyeop@users.noreply.github.com>
2025-04-29 09:21:03 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -4,11 +4,12 @@ class Solution {
         return answer;
     }
     
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+    
     public static int gcd(int a, int b) {
         if (b == 0) return a;
         return gcd(b, a % b);
-    }
-    public static int lcm(int a, int b) {
-        return a * b / gcd(a, b);
     }
 }
